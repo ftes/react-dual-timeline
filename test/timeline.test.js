6a1cb26fcaef72fed2cef9c907c1fba8fcf089c9
Fredrik Teschke <f@ftes.de>
2018-02-06 00:16:18 +0100

    [fix] test error after version bump

--- test/timeline.test.js
+++ test/timeline.test.js
@@ -1,5 +1,8 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { shallow, configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+configure({ adapter: new Adapter() })
 
 import Timeline from '../src'
 
